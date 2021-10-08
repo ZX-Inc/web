b275aa0ae7e35bef6e88259acef92242a969a3a8
Charles <ritaswc@163.com>
2021-10-08 15:01:09 +0800

    未完成

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -10,6 +10,9 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 namespace App\Controller;
+use ZxInc\Zxipdb\IPTool;
+use ZxInc\Zxipdb\IPv4Tool;
+use ZxInc\Zxipdb\IPv6Tool;
 use function Hyperf\ViewEngine\view;
 
 class IndexController extends AbstractController
@@ -28,6 +31,11 @@ class IndexController extends AbstractController
     {
         $type = $this->request->input('type', 'xml');
         $ip = $this->request->input('ip');
+        if (!(IPv4Tool::isValidAddress($ip) || IPv6Tool::isValidAddress($ip))) {
+            $ip = '8.8.8.8';
+        }
+        return $this->response->json(IPTool::query($ip));
+
         switch ($type) {
             case 'json':
 
